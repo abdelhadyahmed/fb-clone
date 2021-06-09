4b5ef6c553bcc5d7f779bcf592d4da6be8dbeed1
unknown <pc@DESKTOP-LKTOJIL.ghalahcity.home>
2021-06-09 08:53:40 +0200

    done

---
+++ src/Header.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import { StorefrontOutlined, SubscriptionsOutlined, SupervisedUserCircle } from '@material-ui/icons';
+import { Avatar, IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://img.icons8.com/fluent/240/000000/facebook-new.png" alt=""/>
+                <div className="Header__input">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+            <div className="header__middle">
+                <div className="header__option">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlined fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlined fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircle fontSize="large" />
+                </div>
+            </div>
+
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Hady Ahmed</h4>
+                </div>
+
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDwKB8pKieOB0q61rF0HSaN5V9su65ZC7k",
+    authDomain: "facebook-clone-2fba1.firebaseapp.com",
+    projectId: "facebook-clone-2fba1",
+    storageBucket: "facebook-clone-2fba1.appspot.com",
+    messagingSenderId: "22319383320",
+    appId: "1:22319383320:web:389eff770cafe299ae946d",
+    measurementId: "G-LLJM9WE77Q"
+  };
